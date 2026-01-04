0390f44ac0e75dad2eb7f3304719dd31ced8feb8
Jaisurya-dot <jaibrother18@gmail.com>
2026-01-04 17:34:52 +0530

    Update db_config.py

--- db/db_config.py
+++ db/db_config.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 # Load .env from current directory or parent directory
 load_dotenv()
 if not os.getenv("DATABASE_URL"):
-    load_dotenv(os.path.join(os.path.dirname(__file__), "..", "..", ".env"))
+    load_dotenv(os.path.join(os.path.dirname(__file__), "..", ".env"))
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
